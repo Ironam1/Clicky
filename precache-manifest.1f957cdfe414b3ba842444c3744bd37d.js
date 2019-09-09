55f4caf0be3b1222e3aa390c10a39b9a61761155
Ironam1 <ironam1@icloud.com>
2019-09-08 20:12:59 -0400

    Updates

---
+++ precache-manifest.1f957cdfe414b3ba842444c3744bd37d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c05e1dc2e6ddae822197c664f30cd3",
+    "url": "/Clicky/index.html"
+  },
+  {
+    "revision": "d857bbfef133e9bb83e8",
+    "url": "/Clicky/static/css/main.2d8640c6.chunk.css"
+  },
+  {
+    "revision": "515c38e9a345c80ac37a",
+    "url": "/Clicky/static/js/2.87582209.chunk.js"
+  },
+  {
+    "revision": "d857bbfef133e9bb83e8",
+    "url": "/Clicky/static/js/main.c012697a.chunk.js"
+  },
+  {
+    "revision": "d95753e291f80f1b1169",
+    "url": "/Clicky/static/js/runtime~main.f550e08a.js"
+  }
+]);
